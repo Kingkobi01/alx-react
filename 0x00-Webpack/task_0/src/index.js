4809d5ef2eec8975e3b2d875e275b39bde864ec9
Kwabena Boakye <kwaboakye93@gmail.com>
2024-01-01 08:36:46 +0000

    Basic setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,13 @@
+import $ from "jquery";
+
+const paragraphs = [
+  "Holberton Dashboard",
+  "Dashboard data for the students",
+  "Copyright - Holberton School",
+];
+
+paragraphs.forEach((paragraph) => {
+  $("body").append(`<p>${paragraph}</p>`);
+});
+
+console.log($("body").textContent);
