0a0686ffe9f282ff49eec4a1a4b7337e2b1006fc
Kwabena Boakye <kwaboakye93@gmail.com>
2024-01-03 11:44:11 +0000

    Modify the Notifications

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -16,10 +16,24 @@ function App() {
 
       <div className="App-body">
         <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email </label>
-        <input type="email" name="email" id="email" /> <br />
-        <label htmlFor="password">Password </label>
-        <input type="password" name="password" id="password" />
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          style={{
+            marginRight: "1em",
+          }}
+          name="email"
+          id="email"
+        />
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          style={{
+            marginRight: "1em",
+          }}
+          name="password"
+          id="password"
+        />
         <button>OK</button>
       </div>
 
