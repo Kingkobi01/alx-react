61e5ae2748e358e214d0e35c6744ceb9981e9349
Kwabena Boakye <kwaboakye93@gmail.com>
2024-01-05 00:03:15 +0000

    Updates

--- 0x01-react_intro/task_1/dashboard/src/Notifications.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Notifications.css";
-export const Notifications = () => (
-  <div className="Notifications">
-    <p>Here is the list of notifications</p>
-  </div>
-);
